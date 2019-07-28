cae8ec0c23e528ef6de8696cbe63fb4bea125373
Rashy98 <rashinikavindya@gmail.com>
2019-07-28 09:58:50 +0530

    interface02 (level 04)..

---
+++ app/src/main/java/com/example/a4t4wmlv4/level04S2.java
@@ -0,0 +1,14 @@
+package com.example.a4t4wmlv4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class level04S2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level04_s2);
+    }
+}
