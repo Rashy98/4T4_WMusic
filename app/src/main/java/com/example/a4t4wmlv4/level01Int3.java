e88060af0f04f6fd33d915dd96df239fcf276179
Nisansali <nisansali97@gmail.com>
2019-07-30 10:28:36 +0530

    level01 (Interface 3)

---
+++ app/src/main/java/com/example/a4t4wmlv4/level01Int3.java
@@ -0,0 +1,14 @@
+package com.example.a4t4wmlv4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class level01Int3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level01_int3);
+    }
+}
--- app/src/main/java/com/example/a4t4wmlv4/level1.java
+++ app/src/main/java/com/example/a4t4wmlv4/level1.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewPropertyAnimator;
 import android.widget.ImageView;
 
 public class level1 extends AppCompatActivity {
