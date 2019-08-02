a916626802bd85d4b477aa8a238605815d5140bc
Nisansali <nisansali97@gmail.com>
2019-08-02 22:07:26 +0530

    add level 1 interface 4

--- app/src/main/java/com/example/a4t4wmlv4/MainActivity.java
+++ app/src/main/java/com/example/a4t4wmlv4/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.media.MediaPlayer;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1);
+
+        MediaPlayer mplayer = MediaPlayer.create(this,R.raw.level1music);
+
+        mplayer.start();
     }
 }
--- app/src/main/java/com/example/a4t4wmlv4/level1.java
+++ app/src/main/java/com/example/a4t4wmlv4/level1.java
@@ -4,14 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewPropertyAnimator;
+import android.widget.Button;
 import android.widget.ImageView;
 
+
 public class level1 extends AppCompatActivity {
+
+
+    Button b1;
+    ImageView iv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1);
+
+        clickfunction();
+    }
+
+
+    public void clickfunction() {
+
+
+
+        b1 = (Button) findViewById(R.id.button10);
+
+        b1.setOnClickListener(new View.OnClickListener(){
+
+        public void onClick(View arg0){
+
+            iv1.setImageResource(R.drawable.correct);
+        }
+
+        });
+
     }
 }
