b3ebe5f1645eab0b28ed9859d9aeeda00cb5858f
Nippa07 <nip.manoratne@gmail.com>
2019-08-03 03:18:21 +0530

    commited

---
+++ app/src/main/java/com/example/a4t4wmlv4/level03_int01.java
@@ -0,0 +1,14 @@
+package com.example.a4t4wmlv4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class level03_int01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level03_int01);
+    }
+}
