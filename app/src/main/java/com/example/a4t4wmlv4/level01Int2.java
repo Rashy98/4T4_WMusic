9c027bfade44a7133fb804c7ea5fc0d47127dee8
HiruSam <hirusamarawickrama824hs@gmail.com>
2019-09-19 11:25:12 +0530

    Update level 02 dfdff

--- app/src/main/java/com/example/a4t4wmlv4/level01Int2.java
+++ app/src/main/java/com/example/a4t4wmlv4/level01Int2.java
@@ -3,8 +3,11 @@ package com.example.a4t4wmlv4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.text.format.DateUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -122,8 +125,16 @@ public class level01Int2 extends AppCompatActivity {
         toast.setDuration(Toast.LENGTH_SHORT);
         toast.setView(layout);
         toast.show();
+        Vibratee();
 
 
+    }
+    private void Vibratee() {
+        if (Build.VERSION.SDK_INT >= 26) {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(VibrationEffect.createOneShot(150,10));
+        } else {
+            ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(150);
+        }
     }
 
 }
